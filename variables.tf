2ac674d9929f35be216cafa6362793793c73cffa
Mario IC <mario21ic@gmail.com>
2023-03-29 01:02:10 -0500

    Ready

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name_prefix" {
+    type = string
+    description = "Nombre del project"
+    default = "demo"
+}
+
+variable "resource_group_location" {
+    type = string
+    description = "Location of project"
+    default = "eastus"
+}
+
+
