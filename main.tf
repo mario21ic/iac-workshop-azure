77d9d590cab5e82b7b4a3fd6df1517a7940dceac
Mario IC <mario21ic@gmail.com>
2023-04-12 15:59:04 -0500

    feat(global) updating

--- main.tf
+++ main.tf
@@ -3,15 +3,16 @@ resource "azurerm_resource_group" "myrg" {
     name = "${var.resource_group_name_prefix}_myrg"
     location = var.resource_group_location
 }
-*/
-
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
+*/
+
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  //name     = random_pet.rg_name.id
+  name     = "miresgroup"
 }
 
 # Create virtual network
