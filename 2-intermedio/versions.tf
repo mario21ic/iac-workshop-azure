0e949cfec89546d1bbba973ff794e311565238a2
Mario IC <mario21ic@gmail.com>
2023-04-25 23:42:09 -0500

    feat(intermedio) actualizando versions

---
+++ 2-intermedio/versions.tf
@@ -0,0 +1,27 @@
+terraform {
+
+  # Configurar backend
+  backend "azurerm" {
+    resource_group_name  = "minuevorg"
+    storage_account_name = "minuevorgtfdemo1"
+    # container_name       = "intermedio"
+    container_name       = "tmp"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.43.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+}
+
